027d6f88a82b28a6e63633e6dad1e61fd9c80da8
Lucas Richter <richterlucas@hotmail.com>
2018-05-17 23:56:21 -0300

    tela inicial começadno

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var app = express()
 app.set( 'view engine', 'pug' )
 
 app.use( '/', express.static( 'dest/' ) )
-app.use( '/image', express.static( 'dest/image' ) )
+app.use( '/images', express.static( 'dest/images' ) )
 app.use( '/css', express.static( 'dest/css' ) )
 app.use( '/static/js', express.static( 'dest/js' ) )
 
 app.use( '*', ( req, res ) => {
-  res.sendFile( __dirname + '/dest/index.html' )
+  res.sendFile( __dirname + '/dest/home.html' )
 } )
 
-var server = app.listen( 8081, function () {
+var server = app.listen( 80, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Server listening at http://localhost:8081/")
+  console.log("Server listening at http://localhost/")
 } )
